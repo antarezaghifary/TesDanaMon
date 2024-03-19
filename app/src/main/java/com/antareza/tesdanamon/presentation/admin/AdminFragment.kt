bd0819f46b863d08360ff7244bd51c1216befc6b
Antareza <oneakun08@gmail.com>
2024-03-19 10:43:35 +0700

    [:feat - add dialog confirmation when delete data]

--- app/src/main/java/com/antareza/tesdanamon/presentation/admin/AdminFragment.kt
+++ app/src/main/java/com/antareza/tesdanamon/presentation/admin/AdminFragment.kt
@@ -1,16 +1,22 @@
 package com.antareza.tesdanamon.presentation.admin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.antareza.tesdanamon.R
+import com.antareza.tesdanamon.data.reqres.model.User
 import com.antareza.tesdanamon.databinding.FragmentAdminBinding
 import com.antareza.tesdanamon.presentation.base.BaseFragment
 import com.antareza.tesdanamon.util.SharedPref
+import com.antareza.tesdanamon.util.setError
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -29,7 +35,7 @@ class AdminFragment : BaseFragment<FragmentAdminBinding>(FragmentAdminBinding::i
             bundle.putParcelable(DATA, editData)
             navigation?.navigate(R.id.action_adminFragment_to_adminEditUserFragment, bundle)
         }, { deleteData ->
-            viewModel.deleteDataUser(deleteData.id)
+            showDeleteConfirmationDialog(deleteData)
         })
     }
     private val viewModel: AdminViewModel by viewModel()
@@ -92,6 +98,30 @@ class AdminFragment : BaseFragment<FragmentAdminBinding>(FragmentAdminBinding::i
                     ActivityCompat.finishAffinity(requireActivity())
                 }
             })
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private fun showDeleteConfirmationDialog(user: User) {
+        val dialogView =
+            LayoutInflater.from(requireContext()).inflate(R.layout.dialog_confirm, null)
+        val passwordEditText = dialogView.findViewById<EditText>(R.id.tie_password)
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.delete_confirmation_title))
+            .setMessage(getString(R.string.delete_confirmation_message))
+            .setView(dialogView)
+            .setPositiveButton(getString(R.string.delete)) { _, _ ->
+                val password = passwordEditText.text.toString()
+                if (password == userSharedPref.getDataUser().password) {
+                    viewModel.deleteDataUser(user.id)
+                } else {
+                    setError(getString(R.string.password_not_match), requireContext())
+                }
+            }
+            .setNegativeButton(getString(R.string.cancel), null)
+            .create()
+
+        dialog.show()
     }
 
     companion object {
